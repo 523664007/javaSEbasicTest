6665b54f73bb9cbd45f7010647f3b33c97805b30
gjy <523664007@qq.com>
2019-08-05 20:16:37 +0800

    homework

---
+++ PerfectNum.java
@@ -0,0 +1,15 @@
+public class PerfectNum{
+	public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++){
+            int x = 0;
+            for (int j = 1; j < i; j++) {
+                if  (i % j == 0) {
+			     x = x + j;
+			    }
+			}if (x == i) {
+                    System.out.println(i);
+                }
+        }
+    }
+    
+}
---
+++ Test14.java
@@ -0,0 +1,14 @@
+//¾Å¾Å³Ë·¨±í
+public class Test14{
+	public static void main(String[] args){
+		int i = 9;
+		for(int m =1;m <= i;m++){
+			for(int n = 1;n <= m;n++){
+
+					System.out.print(n+"*"+m+"="+n*m+" ");
+			}
+			 System.out.print("\n");
+		}
+		
+	}
+}
