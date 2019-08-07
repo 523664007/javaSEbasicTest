30146e4a492a5e03b66a6c66cba510d012de1ef2
523664007 <53599036+523664007@users.noreply.github.com>
2019-08-07 19:51:48 +0800

    game2

--- GamePlayer.java
+++ GamePlayer.java
@@ -1,57 +1,36 @@
-package com.neuedu.test;
+package com.y.test;
+import java.util.*;
 
-import java.util.Random;
-
-/**
- * @author neuedu_yhl
- * @version 1.0
- * @since 08.07
- * */
 public class GamePlayer {
-
-	// 姓名
-	public String name;
-	
-	// 性别
-	public char sex;
-	
-	// 种族
-	public int race;
-	
-	// 职业
-	public String professional;
-	
-	
-	// 力量
-	public int power;
-	
-	
-	// 敏捷
-	public int agile;
-	
-	// 体力
-	public int physicalPower;
-	
-	// 智慧
-	public int wisdom;
-	
-	// 智力
-	public int intelligence;
-	
-	// 生命值
-	public int hp;
-	
-	// 魔法值
-	public int mp;
-	
-	
-	// 随机生成力量的方法
-	public int getPower() {
-		Random random = new Random();
-		int nextInt = random.nextInt(10000);
-		return nextInt;
-	}
-	
-	// 其它生成的方法。。。
-	
+   public String name;                  //名字
+   public int sex;                      //性别
+   public int race;                     //种族
+   public String career;                //职业
+   public int power;                    //力量
+   public int dex;                      //敏捷
+   public int wit;                      //智力
+   public int wisdom;                   //智慧
+   public int hp;                       //生命值
+   public int mp;                       //魔法值
+ 
+     public int getPower() { 	
+    	 Random random = new Random();
+ 		int nextInt = random.nextInt(10000);
+ 		return nextInt;
+}
+     public int getDex() { 	
+    	 Random random = new Random();
+ 		int nextInt = random.nextInt(10000);
+ 		return nextInt;
+}
+     public int getWit() { 	
+    	 Random random = new Random();
+ 		int nextInt = random.nextInt(10000);
+ 		return nextInt;
+}
+     public int getWisdom() { 	
+    	 Random random = new Random();
+ 		int nextInt = random.nextInt(10000);
+ 		return nextInt;
+}
 }
