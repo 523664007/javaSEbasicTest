5315de1de1bd574f6e7853c623774210ef06c47d
523664007 <53599036+523664007@users.noreply.github.com>
2019-08-07 19:51:23 +0800

    game

--- Client.java
+++ Client.java
@@ -1,90 +1,80 @@
-package com.neuedu.test;
+package com.y.test;
 
-import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.*;
 
-/**
- * 客户端
- * @author neuedu_yhl
- */
 public class Client {
-	
 	public static void main(String[] args) {
 		welcome();
 	}
+	public static void welcome(){
+		 System.out.println("这是一个RPG游戏：");
+		 System.out.println("1.开始游戏");
+		 System.out.println("2.游戏规则");
+		 System.out.println("3.结束游戏");
+	     System.out.println("请输入您想进行的操作：");
+		     Scanner scanner = new Scanner(System.in);
+		     int num = scanner.nextInt();
+		     switch (num) {
+		        case 1:
+			        createNewGame();
+			        break;
+		        case 2:
+		             gameRule();
+		             welcome();
+		            break;
+		        case 3:
+			       gameOver();
+			        break ;
+			    default :
+			    	System.out.println("输入有误，请重新输入");
+			    	welcome();
+			    	break;
+		     }
+		 }
 	
-
-    // 写一个方法 输出欢迎界面
-	public static void welcome() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("============================");
-		System.out.println("==============欢迎来到王者荣耀...==============");
-		System.out.println("============================");
-		System.out.println("1.开始游戏...");
-		System.out.println("2.游戏规则...");
-		System.out.println("3.结束游戏...");
-		System.out.println("请输入您想进行的操作:(1-3)");
-		int nextInt = scanner.nextInt();
-		switch (nextInt) {
-		case 1:
-			// 执行开始游戏
-			createNewGame();
-			break;
-
-		case 2:
-			// 执行输出游戏规则
-			gameRules();
-			welcome();
-			break;
-		case 3:
-			//执行结束游戏
-			gameOver();
-			break;
-		default:
-			System.out.println("输入有误,请重新输入");
-			welcome();
-			break;
+	    
+		public static void createNewGame(){
+			GamePlayer[]players = {};
+			Scanner scanner = new Scanner(System.in);
+	        System.out.println("请输入您的游戏名字：");
+					Scanner scanner1 = new Scanner(System.in);
+				    String name = scanner1.next();
+					System.out.println("请输入您的游戏角色的种族： 1.人类 2.精灵 3.兽人 4.小矮人 5.元素 ");
+					Scanner scanner2 = new Scanner(System.in);
+					int race= scanner2.nextInt();
+					
+					System.out.println("请角色选择性别：1.男 2.女");	
+					Scanner scanner3 = new Scanner(System.in);
+					int sex = scanner3.nextInt();
+					    
+			      GamePlayer gp = new GamePlayer();
+			      gp.name = name;
+  		          gp.race = race;
+		          gp.sex = sex;
+			      GamePlayer[] copyOf = Arrays.copyOf(players,players.length+1);
+			      copyOf[copyOf.length-1] = gp;
+			      if(copyOf !=null) {
+			    	  System.out.println("创建成功");
+				      System.out.println("您的英雄名叫:"+gp.name);
+				      System.out.println("您的英雄种族叫:"+gp.race);
+				      System.out.println("您的英雄性别是:"+gp.sex);
+				      int power = gp.getPower();
+				      System.out.println("您的英雄力量初始为:"+power);
+				      int dex = gp.getDex();
+				      System.out.println("您的英雄敏捷初始为:"+dex);
+				      int wit = gp.getWit();
+				      System.out.println("您的英雄智力初始为:"+wit);
+				      int wisdom = gp.getWisdom();
+         		      System.out.println("您的英雄智慧初始为:"+wisdom);
+			      }
+			      welcome();
+	}
+		public static void gameRule () {
+			System.out.println("创建角色，HP和MP初始值为1000，力量、敏捷、智慧、智力随机分配");
 		}
-	}
-	/*
-	 * 请输入您游戏角色的姓名:李白  
-	      请输入游戏角色的种族:(1.人类 2.精灵 3.兽人 4.矮人 5.元素)
-	      创建成功!!!
-	 * */
-	// 创建开始游戏的方法
-	public static void createNewGame() {
-		// 创建一个容器来存放角色信息
-		// 数组
-		GamePlayer[] players = {};
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("请输入您游戏角色的姓名:");
-		String name = scanner.next();
-		System.out.println("请输入游戏角色的种族:(1.人类 2.精灵 3.兽人 4.矮人 5.元素)");
-	    int race = scanner.nextInt();
-	    // 构建游戏角色
-	    GamePlayer gp = new GamePlayer();
-	    gp.name = name;
-	    gp.race = race;
-	    // 游戏角色容器长度+1
-	    GamePlayer[] copyOf = Arrays.copyOf(players, players.length+1);
-	    copyOf[copyOf.length-1] = gp;
-	    // 判断
-	    if(copyOf != null) {
-	    	System.out.println("创建成功");
-	    	System.out.println("您的英雄名叫:"+gp.name);
-	    	System.out.println("您的英雄种族叫:"+gp.race);
-	    	int power = gp.getPower();
-	    	System.out.println("您的英雄力量初识为:"+power);
-	    }
-	    welcome();
-	}
-	// 创建游戏规则的方法
-	public static void gameRules() {
-		System.out.println("这是游戏规则》。。。");
-	}
-	
-	// 结束游戏的方法
-	public static void gameOver() {
-		System.exit(0);
-	}
+		public static void gameOver() {
+			System.exit(0);
+		}
 }
